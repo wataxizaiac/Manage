6d4e18e4fbdd672cf0c782b7b97c3fe785a18d8a
17607271623 <1764642535@qq.com>
2022-09-23 19:59:34 +0800

    规范

--- vite-project/src/axios/api.ts
+++ vite-project/src/axios/api.ts
@@ -1,42 +1,42 @@
-import service from "@/axios/index";
-import {LoginFormData} from "@/types/login";
+import service from '@/axios/index'
+import { LoginFormData } from '@/types/login'
 
 // 登录接口
 export function login(data: LoginFormData) {
-    return service({
-        url: `/login?username=${data.username}&password=${data.password}`,
-        method: "GET"
-    })
+  return service({
+    url: `/login?username=${data.username}&password=${data.password}`,
+    method: 'GET'
+  })
 }
 
 // 商品列表接口
-export function getGoodsList(){
-    return service({
-        url: "/list",
-        method: "POST"
-    })
+export function getGoodsList() {
+  return service({
+    url: '/list',
+    method: 'POST'
+  })
 }
 
 // 用户列表接口
-export function getUserList(){
-    return service({
-        url: "/getUserList",
-        method: "GET"
-    })
+export function getUserList() {
+  return service({
+    url: '/getUserList',
+    method: 'GET'
+  })
 }
 
 // 角色列表接口
-export function getRoleList(){
-    return service({
-        url: "/getRoleList",
-        method: "GET"
-    })
+export function getRoleList() {
+  return service({
+    url: '/getRoleList',
+    method: 'GET'
+  })
 }
 
 // 权限列表接口
-export function getAuthorityList(){
-    return service({
-        url: "/getAuthorityList",
-        method: "GET"
-    })
+export function getAuthorityList() {
+  return service({
+    url: '/getAuthorityList',
+    method: 'GET'
+  })
 }
--- vite-project/src/axios/index.ts
+++ vite-project/src/axios/index.ts
@@ -1,28 +1,28 @@
-import router from "@/router";
-import axios from "axios";
-//创建axios实例
+import router from '@/router'
+import axios from 'axios'
+// 创建axios实例
 const service = axios.create({
-  baseURL: "http://127.0.0.1:3000",
+  baseURL: 'http://127.0.0.1:3000',
   timeout: 5000,
   headers: {
-    "Content-type": "application/json;charset=utf-8"
+    'Content-type': 'application/json;charset=utf-8'
   }
 })
 
-//请求拦截
+// 请求拦截
 service.interceptors.request.use(
   config => {
     config.headers = config.headers || {}
-    if (localStorage.getItem("token")) {
-      config.headers.token = localStorage.getItem("token") || ""
+    if (localStorage.getItem('token')) {
+      config.headers.token = localStorage.getItem('token') || ''
     }
     return config
   })
 
-//响应拦截
+// 响应拦截
 service.interceptors.response.use(
   res => {
-    const code: number = res.data.code
+    const { code } = res.data
     if (code != 200) {
       return Promise.reject(res.data)
     }
@@ -31,7 +31,7 @@ service.interceptors.response.use(
   err => {
     // console.log(err);
     const code: number = err.response.status
-    if(code === 999){
+    if(code === 999) {
       router.replace('/login')
     }
     return Promise.reject(err.response)
